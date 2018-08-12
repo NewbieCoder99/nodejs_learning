a2bf4acc1c78995a664d994a699d2e739ba3c756
NewbieCoder18 <fajarpunya99@gmail.com>
2018-08-13 02:12:10 +0700

    Try Session Login

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,7 +1,3 @@
-const   mysql   = require('mysql'),
-	  	config  = require('../config/config.json'),
-	  	conn 	= mysql.createConnection(config);
-
 exports.index = function(req, res, next) {
 	res.send(req.session.userdata);
 }
