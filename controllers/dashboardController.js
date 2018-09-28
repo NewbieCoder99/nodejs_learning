9fd3352c68234c3067ea864394dfc7adfedee5cd
Newbie Coder <fajarpunya99@gmail.com>
2018-09-28 14:07:33 +0700

    Fix Dashboard Template

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,6 +1,9 @@
 exports.index = function(req, res, next) {
 	if(req.session.userdata != null) {
-		res.render('dashboard', { title: 'Dashboard admin', sess : req.session.userdata });
+		res.render('adminpanel/adminpanel_index', {
+			title 	: 'Dashboard admin',
+			sess 	: req.session.userdata
+		});
 	} else {
 		res.redirect('/');
 	}
