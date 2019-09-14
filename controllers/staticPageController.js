632024558854d12c9f5ce9beb0b65bd5d705382b
Newbie Coder <fajarpunya99@gmail.com>
2019-09-14 15:52:46 +0700

    Parsing My Profile On Upwork

--- controllers/staticPageController.js
+++ controllers/staticPageController.js
@@ -1,8 +1,23 @@
+var HTMLParser = require('node-html-parser');
+var http = require("http");
+var request = require('request');
+
 exports.about = function(req, res, next) {
-	return res.render('about', {
-		title : 'About Me',
-		segment : 'about'
+
+	request.get({
+		url: 'https://www.upwork.com/fl/qudratnurfajarys',
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+			'Content-Type' : 'application/x-www-form-urlencoded' 
+		}
+	}, function (e, r, body) {
+		console.log(body)
+		return res.render('about', {
+			title : 'About Me',
+			segment : 'about'
+		});
 	});
+
 }
 
 exports.contact = function(req, res, next) {
